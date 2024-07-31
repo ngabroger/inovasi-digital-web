dedc313c42239edfd9e716cb7d46cbf70249bc8b
ngabroger <arbination13@gmail.com>
2024-07-31 22:28:05 +0700

    update animation

--- resources/js/animation.js
+++ resources/js/animation.js
@@ -5,7 +5,7 @@ const paths = document.querySelectorAll('#stairs path');
 gsap.timeline()
     .from(paths, {
         duration: .1,
-        stagger: 0.1, // Animasi berurutan
+        stagger: 0.009, // Animasi berurutan
         scale: 0, // Mulai dari skala 0 (tak terlihat)
         transformOrigin: "center", // Transformasi dari tengah elemen
         opacity: 0, // Mulai dari opasitas 0
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,8 @@ module.exports = {
     extend: {
       animation: {
         "loop-scroll": "loop-scroll 25s linear infinite",
-         'loop-scroll-reverse': 'loop-scroll-reverse 20s linear infinite'
+         'loop-scroll-reverse': 'loop-scroll-reverse 20s linear infinite',
+         'float': 'float 3s ease-in-out infinite'
       },
       keyframes: {
         "loop-scroll": {
@@ -18,8 +19,15 @@ module.exports = {
         'loop-scroll-reverse': {
           from: { transform: 'translateX(-100%)' },
           to: { transform: 'translateX(100%)' }
+        },
+        float: {
+          '0%, 100%': {
+            transform: 'translateY(-10%)'
+          },
+          '50%': {
+            transform: 'translateY(-5%)'
+          }
         }
-        
       },
     },
   },
